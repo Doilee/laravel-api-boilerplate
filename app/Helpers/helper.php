39d6a1ba3b71511a5d6c7d6a1219538de7d0c6d8
Matthijs <matthijs@phoenixsite.nl>
2019-03-01 16:14:00 +0100

    Added me() helper function

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,1 +1,16 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
+
+if (!function_exists('me'))
+{
+    /**
+     * Returns the logged in user.
+     *
+     * @return \App\User|null
+     */
+    function me() : ?\Illuminate\Contracts\Auth\Authenticatable
+    {
+        return Auth::user();
+    }
+}
